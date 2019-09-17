078d78c400c45966be0de95acc36160faec5eeda
Yevgeniy Ivanov <anigeka@gmail.com>
2019-09-17 17:58:04 -0400

    initial commit

---
+++ ChatApplication/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Npgsql;
+using UserDataAccess;
+
+namespace ChatApplication.Controllers
+{
+    public class UsersController : ApiController
+    {
+        UserDataAccess.DataBaseAccess db = new UserDataAccess.DataBaseAccess();
+        // GET: api/Users
+        public List<string> Get()
+        {
+            List<string> dt = db.MakeRequest();
+
+            return dt;
+        }
+
+        // GET: api/Users/5
+        public string Get(int id)
+        {
+
+            return "value";
+        }
+
+        // POST: api/Users
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserDataAccess/DataBaseAccess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace UserDataAccess
+{
+    public class DataBaseAccess
+    {
+        private NpgsqlConnection conn;
+        private string connstring = String.Format("Server={0};Port={1}, " +
+            "User Id={2};Password={3};Database={4};", "localhost", "5432", "postgres",
+            "MYPASS", "chatapp");
+        private DataTable dt;
+        private NpgsqlCommand comm;
+        private string sql = null;
+
+        public List<string> MakeRequest()
+        {
+            try
+            {
+                /*conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                sql = "select * from get_users()";
+                comm = new NpgsqlCommand(sql, conn);
+
+                dt = new DataTable();
+                dt.Load(comm.ExecuteReader());
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(comm);
+                adapter.Fill(dt);
+
+                List<string> names = new List<string>();
+                foreach(DataRow row in dt.Rows)
+                {
+                    names.Add(row["name"].ToString());
+                }
+                return names;
+                */
+                return new List<string>() { "hi", "hello" };
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+            finally
+            {
+               
+            }
+            return null;
+        }
+        
+    }
+}
